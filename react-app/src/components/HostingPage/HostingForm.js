b302155cfd61eb418a1d77d47deb31059de61073
wilson <whuang3015@gmail.com>
2021-07-31 04:09:27 -0700

    make hosting form

--- react-app/src/components/HostingPage/HostingForm.js
+++ react-app/src/components/HostingPage/HostingForm.js
@@ -1,19 +1,38 @@
 import React, { useState } from 'react';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { useHistory } from 'react-router';
+
+import { hostMeeting } from '../../store/meeting';
+import Button from '../button';
+import styles from './HostingForm.module.css'
 
 export default function HostingForm() {
   const history = useHistory();
+  const dispatch = useDispatch();
 
   const user = useSelector((state) => state.session.user)
   const host_id = user?.id
 
-  const [name, setName] = useState("")
-  const [description, setDescription] = useState("")
-  const [queueLimit, setQueueLimit] = useState("")
+  const [errors, setErrors] = useState([]);
+  const [name, setName] = useState("");
+  const [description, setDescription] = useState("");
+  const [queue_limit, setQueueLimit] = useState("");
 
-  const handleSubmit = () => {
-    return
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const payload = {
+      host_id,
+      name,
+      description,
+      queue_limit
+    }
+    const response = await dispatch(hostMeeting(payload))
+    if (response) {
+      setErrors(response)
+      history.push(`/meetings/${response['id']}`)
+    } else {
+      errors.push("Meeting could not be created.")
+    }
   }
 
   const updateMeetingName = (e) => {
@@ -29,13 +48,16 @@ export default function HostingForm() {
   }
 
   return (
-    <div>
-      <form onSubmit={handleSubmit}>
+    <div className={styles.formContainer}>
+      <form className={styles.form} onSubmit={handleSubmit}>
         <input name="host" type="hidden" value={host_id}/>
         <div>
           <input
+            className={styles.inputField}
             name='name'
             type='text'
+            minlength="2"
+            maxlength="64"
             placeholder='Meeting Title'
             onChange={updateMeetingName}
             value={name}
@@ -43,23 +65,45 @@ export default function HostingForm() {
         </div>
         <div>
           <input
+            className={styles.inputField}
             name='description'
             type='text'
+            minlength="1"
+            maxlength="1000"
             placeholder='Description'
             onChange={updateDescription}
             value={description}
-          />
+            />
         </div>
         <div>
           <input
+            className={styles.inputField}
             name='queue limit'
             type='number'
+            min="1"
+            max="25"
             placeholder='Queue Limit'
             onChange={updateQueueLimit}
-            value={queueLimit}
+            value={queue_limit}
           />
         </div>
-
+        <button type="submit">
+          <Button
+          action={handleSubmit}
+          borderRadius={10}
+          btnColor={"gold"}
+          text={"Host"}
+          fontColor={"black"}
+          fontSize={16}
+          />
+        </button>
+        <div className={styles.errorsContainer}>
+          {errors.map((error, ind) => (
+            <div className={styles.error} key={ind}>
+              {error}
+            </div>
+          ))}
+        </div>
       </form>
     </div>
   )
--- react-app/src/components/HostingPage/index.js
+++ react-app/src/components/HostingPage/index.js
@@ -2,11 +2,13 @@ import React from 'react';
 import { useSelector } from 'react-redux';
 import HostingForm from './HostingForm';
 
+import styles from './HostingForm.module.css'
+
 export default function HostingPage() {
 
   return (
-    <>
+    <div className={styles.pageContainer}>
       <HostingForm/>
-    </>
+    </div>
   )
 }
