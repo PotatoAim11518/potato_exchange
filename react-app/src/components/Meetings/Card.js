0d9eae9e2a8f988efeb7abfe9de4cc15cdc17c12
wilson <whuang3015@gmail.com>
2021-08-09 00:15:07 -0700

    implement dynamic queue sizes on meetings page

--- react-app/src/components/Meetings/Card.js
+++ react-app/src/components/Meetings/Card.js
@@ -1,18 +1,39 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { useHistory } from 'react-router-dom';
+import { allMeetingQueues, getMeetingQueue } from '../../store/queue';
+import socket from '../MeetingPage/socket';
 import styles from './Card.module.css';
 
 export default function Card({ meeting }) {
   const history = useHistory();
+  const dispatch = useDispatch();
 
   const dateOptions = { weekday: 'short', year: 'numeric', month: 'long', day: 'numeric' };
   const timeOptions = { hour: '2-digit', minute: '2-digit' };
   const createdDate = new Date(meeting.created_at).toLocaleDateString('en-US', dateOptions);
   const createdTime = new Date(meeting.created_at).toLocaleTimeString('en-US', timeOptions);
 
+  const queues = useSelector((state) => Object.values(state.queue));
+
   const goToMeeting = () => {
-    history.push(`/meetings/${meeting.id}`)
+    history.push(`/meetings/${meeting?.id}`)
   }
+
+  const calcQueue = (meeting_id) => {
+    let count = 0;
+    queues.forEach(patron => {
+      if (patron['meeting_id'] === meeting_id) ++count
+    });
+    return count
+  }
+
+  useEffect(() => {
+    socket.on('update', () => {
+      dispatch(getMeetingQueue(meeting?.id))
+    })
+    dispatch(allMeetingQueues())
+  },[dispatch, meeting?.id])
 
   return (
     <div className={styles.card} onClick={goToMeeting}>
@@ -21,7 +42,7 @@ export default function Card({ meeting }) {
       <div className={styles.avatar}>{meeting.host.username}</div>
       <p className={styles.description}>{meeting.description}</p>
       <div className={styles.waiting}>
-        <p className={styles.waitingText}><em>Waiting: #/{meeting.queue_limit}</em></p>
+        <p className={styles.waitingText}><em>Waiting: {calcQueue(meeting?.id)}/{meeting.queue_limit}</em></p>
       </div>
     </div>
   )
