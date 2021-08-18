dbccf0779f09013eb5507b69e866592b3b72a04f
wilson <whuang3015@gmail.com>
2021-08-18 02:57:30 -0700

    fix queue styling

--- react-app/src/components/MeetingPage/Meeting/Patron.js
+++ react-app/src/components/MeetingPage/Meeting/Patron.js
@@ -1,45 +1,52 @@
-import React, { useState } from 'react';
-import { useSelector } from 'react-redux';
-import { Modal } from '../../../context/Modal';
-import KickConfirm from './KickConfirm';
-import Button from '../../button';
-import styles from './Patron.module.css'
+import React, { useState } from "react";
+import { useSelector } from "react-redux";
+import { Modal } from "../../../context/Modal";
+import KickConfirm from "./KickConfirm";
+import Button from "../../button";
+import styles from "./Patron.module.css";
 
-
-export default function Patron({patron, meeting}) {
-  const user = useSelector((state) => state.session.user)
-  const user_id = user?.id
-  const userColor = patron.user.id === user_id ? styles.currentUser : styles.otherUser
+export default function Patron({ index, patron, meeting }) {
+  const user = useSelector((state) => state.session.user);
+  const user_id = user?.id;
+  const userColor =
+    patron.user.id === user_id ? styles.currentUser : styles.otherUser;
 
   const [showModal, setShowModal] = useState(false);
 
   const handleKickGuest = () => {
-    setShowModal(true)
-  }
+    setShowModal(true);
+  };
 
   return (
-    <>
-    <span className={userColor}>
-      {patron.user.username}
-    </span>
-    {user_id === meeting?.host_id &&
-      <Button
-        action={handleKickGuest}
-        paddingY={0}
-        paddingX={0}
-        width={40}
-        height={20}
-        borderRadius={2}
-        btnColor={"darkred"}
-        text={"Kick"}
-        fontColor={"white"}
-        fontSize={12}
-      />}
-      {showModal &&
-      <Modal onClose={() => setShowModal(false)}>
-        <KickConfirm patron={patron} meeting={meeting} setShowModal={setShowModal}/>
-      </Modal>
-      }
-      </>
-  )
+    <div className={styles.patronRow}>
+      <div className={styles.patronUser}>
+        <p className={styles.patronIndex}>
+          {index + 1}: <span className={userColor}>{patron.user.username}</span>
+        </p>
+      </div>
+      {user_id === meeting?.host_id && (
+        <Button
+          action={handleKickGuest}
+          paddingY={0}
+          paddingX={0}
+          width={40}
+          height={20}
+          borderRadius={2}
+          btnColor={"darkred"}
+          text={"Kick"}
+          fontColor={"white"}
+          fontSize={12}
+        />
+      )}
+      {showModal && (
+        <Modal onClose={() => setShowModal(false)}>
+          <KickConfirm
+            patron={patron}
+            meeting={meeting}
+            setShowModal={setShowModal}
+          />
+        </Modal>
+      )}
+    </div>
+  );
 }
